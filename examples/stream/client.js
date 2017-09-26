33842436b110adc329b3d45adf744fa166e8f5ac
Fred Chien <cfsghost@gmail.com>
2017-09-26 23:34:16 +0800

    support streaming for client

---
+++ examples/stream/client.js
@@ -0,0 +1,15 @@
+const { Client } = require('../../');
+
+const client = new Client('0.0.0.0', 50051);
+
+client.loadProto(__dirname + '/proto/stream.proto').then(async () => {
+
+	let Example1 = client.getService('example.foo.Example1');
+
+	let stream = await Example1.receive({});
+
+	stream.on('data', (data) => {
+		console.log(data);
+	});
+
+});
